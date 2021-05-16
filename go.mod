dd78994194a204f53254bdcbe3979d2fd53f8fe3
Johnson Jan Su <su1221jan@gmail.com>
2021-05-16 10:12:47 +0800

    update for heroku

--- go.mod
+++ go.mod
@@ -1,5 +1,8 @@
-module github.com/line/line-bot-sdk-go/v7/linebot
+module github.com/bookjan/line-bot-go
 
 go 1.16
 
-require github.com/line/line-bot-sdk-go/v7 v7.9.1 // indirect
+require (
+	github.com/joho/godotenv v1.3.0 // indirect
+	github.com/line/line-bot-sdk-go/v7 v7.9.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/line/line-bot-sdk-go/v7/linebot"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	var port = os.Getenv("PORT")
+	if port == "" {
+		port = "80"
+	}
+
+	secret := os.Getenv("CHANNEL_SECRET")
+	if secret == "" {
+		log.Fatal("CHANNEL_SECRET must be set")
+	}
+
+	token := os.Getenv("CHANNEL_TOKEN")
+	if token == "" {
+		log.Fatal("CHANNEL_TOKEN must be set")
+	}
+
+	bot, err := linebot.New(
+		secret,
+		token,
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Printf("%s\n", r.RequestURI)
+		fmt.Fprintln(w, "Hello, World!")
+	})
+
+	http.HandleFunc("/favicon.ico", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Printf("%s\n", r.RequestURI)
+		w.Header().Set("Content-Type", "image/x-icon")
+		w.Header().Set("Cache-Control", "public, max-age=7776000")
+		fmt.Fprintln(w, "data:image/x-icon;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQEAYAAABPYyMiAAAABmJLR0T///////8JWPfcAAAACXBIWXMAAABIAAAASABGyWs+AAAAF0lEQVRIx2NgGAWjYBSMglEwCkbBSAcACBAAAeaR9cIAAAAASUVORK5CYII=\n")
+	})
+
+	// Setup HTTP Server for receiving requests from LINE platform
+	http.HandleFunc("/callback", func(w http.ResponseWriter, req *http.Request) {
+		events, err := bot.ParseRequest(req)
+		if err != nil {
+			if err == linebot.ErrInvalidSignature {
+				w.WriteHeader(400)
+			} else {
+				w.WriteHeader(500)
+			}
+			return
+		}
+		for _, event := range events {
+			if event.Type == linebot.EventTypeMessage {
+				switch message := event.Message.(type) {
+				case *linebot.TextMessage:
+					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text)).Do(); err != nil {
+						log.Print(err)
+					}
+				case *linebot.StickerMessage:
+					replyMessage := fmt.Sprintf(
+						"sticker id is %s, stickerResourceType is %s", message.StickerID, message.StickerResourceType)
+					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(replyMessage)).Do(); err != nil {
+						log.Print(err)
+					}
+				}
+			}
+		}
+	})
+	// This is just sample code.
+	// For actual use, you must support HTTPS by using `ListenAndServeTLS`, a reverse proxy or something else.
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatal(err)
+	}
+}
